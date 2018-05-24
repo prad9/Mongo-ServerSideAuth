9711a946368ac60d71fe2a4b1643546f317daae4
Pradeep Vattikuti <pradeep.vattikuti@atlantishealthcare.com>
2018-05-25 09:20:47 +1200

    Add cors feature

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const app = express();
 const router = require("./router");
+const cors = require("cors");
 
 //DB setup
 mongoose.connect("mongodb://localhost:27017/admin");
@@ -13,6 +14,7 @@ mongoose.connect("mongodb://localhost:27017/admin");
 //App setup
 //Logging middleware express framework
 app.use(morgan("combined"));
+app.use(cors());
 //Request json converter middleware express framework
 app.use(bodyParser.json({type: "*/*"}));
 router(app);
